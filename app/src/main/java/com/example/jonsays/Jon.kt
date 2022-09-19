f4ae9b929c34f6c38d4c9aaabb5cd0a22630aafd
mayasteiner1 <mayasparkin1@gmail.com>
2022-09-18 20:30:15 -0500

    saved shared preferences

--- app/src/main/java/com/example/jonsays/Jon.kt
+++ app/src/main/java/com/example/jonsays/Jon.kt
@@ -25,6 +25,21 @@ class Jon : Fragment() {
     ): View? {
         _binding = JonBinding.inflate(inflater, container, false);
 
+        //get saved button names
+        binding.title.text = MainActivity.name
+        var pic = MainActivity.image.toUri()
+        binding.image.setImageURI(pic)
+        binding.btn1.text = MainActivity.btn1
+        binding.btn2.text = MainActivity.btn2
+        binding.btn3.text = MainActivity.btn3
+        binding.btn4.text = MainActivity.btn4
+        binding.btn5.text = MainActivity.btn5
+        binding.btn6.text = MainActivity.btn6
+        binding.btn7.text = MainActivity.btn7
+        binding.btn8.text = MainActivity.btn8
+        binding.btn9.text = MainActivity.btn9
+        binding.btn10.text = MainActivity.btn10
+
 
         var mp: MediaPlayer? = null
         var songs = arrayOf(R.raw.champaign)
@@ -58,21 +73,6 @@ class Jon : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        //get saved button names
-        binding.title.text = MainActivity.name
-        var pic = MainActivity.image.toUri()
-        binding.image.setImageURI(pic)
-        binding.btn1.text = MainActivity.btn1
-        binding.btn2.text = MainActivity.btn2
-        binding.btn3.text = MainActivity.btn3
-        binding.btn4.text = MainActivity.btn4
-        binding.btn5.text = MainActivity.btn5
-        binding.btn6.text = MainActivity.btn6
-        binding.btn7.text = MainActivity.btn7
-        binding.btn8.text = MainActivity.btn8
-        binding.btn9.text = MainActivity.btn9
-        binding.btn10.text = MainActivity.btn10
 
     }
 }
